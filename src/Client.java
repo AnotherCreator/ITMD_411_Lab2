e6fef172d1d64adbd445f6ed176b760595c2cc26
Josh <jreginaldo@protonmail.com>
2022-02-04 15:51:59 -0600

    Remake Client.java as abstract class

--- src/Client.java
+++ src/Client.java
@@ -1,32 +1,5 @@
-// This class will handle file parsing and storing
-
-import java.io.*;
-
-public class Client {
-    // Methods
-    public void readData() {
-        BufferedReader br;
-        String line;
-
-        try {
-            br = new BufferedReader(new FileReader("bank-Detail.csv"));
-
-            while ((line = br.readLine()) != null) {
-                System.out.println(line);
-            }
-        }
-        catch (FileNotFoundException e) {
-            System.out.println("File not found");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void processData() {
-
-    }
-
-    public void printData() {
-
-    }
+public abstract class Client {
+    abstract void readData();
+    abstract void processData();
+    abstract void printData();
 }
