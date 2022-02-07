fa71574b23fcd71e192becd94a394cdf03bac2bc
Josh <jreginaldo@protonmail.com>
2022-02-07 10:27:41 -0600

    Add project description

--- src/BankRecordsTest.java
+++ src/BankRecordsTest.java
@@ -6,7 +6,10 @@ Lab 2 - Bank Record Generations
 
 A bank has received a .csv file containing raw data of various clients.
 This lab will have us parse the data and print a record for future loan considerations
-In the end, we will have 600 separate obj
+
+Each row will be stored in an ArrayList as string arrays
+From there, iterate through the entire ArrayList and through each string array
+Create a new BankRecord object per string array and populate each field appropriately using setters
 */
 
 public class BankRecordsTest {
